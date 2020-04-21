4c925da1888e6cf32d282e678149226d5a9ed9b7
gmalsha <malshag.gee@gmail.com>
2020-04-21 16:25:08 +0530

    Constants files added

---
+++ src/main/java/com/upload/server/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.upload.server.utils;
+
+public class Constants {
+	public static final String UPLOAD_ZIP = "Please upload a zip to process";
+	public static final String NO_CONTENT ="No Content Available";
+	public static final String FILE_NOT_FOUND = "File not found";
+	public static final String UNZIPPING_ERROR = "Error occured when unzipping";
+	public static final String CountryNames_URL = "https://restcountries.eu/rest/v2/alpha/";
+	public static final String WebClient_Error = "No matched Names";
+	public static final String SUCCESS = "SUCCESS";
+	public static final String PATH = System.getProperty("java.io.tmpdir");
+	
+}
